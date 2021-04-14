84f157834f3082cba000603556d401c2a6c0f141
HardikParecha <72511914+HardikParecha@users.noreply.github.com>
2021-04-14 21:32:44 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("")
+    textSize(20)
+    text("R E V B A I L A", 100,50)
+    textSize(15)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,80)
+
+    fill("white")
+    textSize(20)
+    text("C U T N I F O N", 700,140)
+    textSize(15)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,170)
+    
+    fill("white")
+    textSize(20)
+    text("A T A D A S B A", 400,290)
+    textSize(15)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 400,320)
+}
